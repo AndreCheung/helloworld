01d7fa53f1e2341beb884a03a87348dc527d9729
AndreCheung <140793321+AndreCheung@users.noreply.github.com>
2023-07-28 15:33:49 +0800

    Create helloworld.py
    A Python program to display Hello World!

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#! python3
+# helloworld.py - print Hello World
+# Last update: 20230728
+# Author: Andre Cheung
+
+print('Hello, world!')
+print('What is your name?')
+name = input()
+print('Nice to meet you, ' + str(name) + '.')
+
+ 
